2ff528b7569d9c069b7f3cbbd9008c839bb30f1b
jeffaraujo <jeff.araujo@gmail.com>
2017-07-31 20:47:11 -0300

    Add project files.

---
+++ Xamarin.ListViewMVVM/Xamarin.ListViewMVVM/View/CarroView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.ListViewMVVM.ViewModel;
+
+namespace Xamarin.ListViewMVVM.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CarroView : ContentPage
+    {
+        public CarroView()
+        {
+            InitializeComponent();
+            this.BindingContext = new CarroViewModel();
+        }
+    }
+}
---
+++ Xamarin.ListViewMVVM/Xamarin.ListViewMVVM/ViewModel/CarroViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.ListViewMVVM.Annotations;
+using Xamarin.ListViewMVVM.Model;
+
+namespace Xamarin.ListViewMVVM.ViewModel
+{
+    public class CarroViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        private ObservableCollection<Carro> items;
+
+        public ObservableCollection<Carro> Items
+        {
+            get { return items; }
+            set { items = value; OnPropertyChanged(nameof(Items)); }
+        }
+
+        public CarroViewModel()
+        {
+            Items = new ObservableCollection<Carro>()
+            {
+                new Carro() {Id = 1, Ano = 2015, Marca = "Volkwagen"},
+                new Carro() {Id = 2, Ano = 2014, Marca = "Tesla Model S"},
+                new Carro() {Id = 3, Ano = 2016, Marca = "Fiat"},
+                new Carro() {Id = 4, Ano = 2017, Marca = "Renault"},
+                new Carro() {Id = 5, Ano = 2016, Marca = "Nissan"}
+            };
+        }
+
+
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
